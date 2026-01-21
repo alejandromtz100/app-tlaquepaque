9ce8bb10103c1994457d68e68b95d26e447e0143
fernandaUTZMG <fernanda.hernandez.22s@utzmg.edu.mx>
2026-01-21 13:45:58 -0600

    merge main

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,25 @@ import Home from "./inicio/home";
 import Tramites from "./catalogos/tramites";
 import Menu from "./layout/menu";
 import Usuarios from "./usuarios/usuarios";
-import Colonias from "./catalogos/Colonias"
+import Colonias from "./catalogos/Colonias";
+import Asignaciones from "./usuarios/asignaciones";
+
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
-        
-        <Route path="/catalogos/tramites" element={<Tramites />} />
-        <Route path="/usuarios" element={<Usuarios />} />
-        <Route path="/layout/menu" element={<Menu />} />
 
-        <Route path="/catalogos/colonias" element={<Colonias />} />
-        <Route path="/layout/menu" element={<Menu />} />
+        <Route path="/tramites" element={<Tramites />} />
+        <Route path="/colonias" element={<Colonias />} />
+        <Route path="/menu" element={<Menu />} />
+
+        <Route path="/usuarios" element={<Usuarios />} />
+        <Route path="/usuarios/asignaciones" element={<Asignaciones />} />
       </Routes>
     </Router>
   );
 };
 
 export default App;
-
